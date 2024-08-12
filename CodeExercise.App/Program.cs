ffeb19f978a246b34619d14f5eb6bde3114c0510
Kurt Schindler <kschindler@fourhands.com>
2024-08-12 10:17:11 -0500

    fix gitignore to include CodeExercise.App

---
+++ CodeExercise.App/Program.cs
@@ -0,0 +1,14 @@
+﻿using CodeExercise.Core;
+
+namespace CodeExercise.App;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var fizzBuzzEngine = new FizzBuzzEngine();
+        fizzBuzzEngine.Run();
+    }
+}
